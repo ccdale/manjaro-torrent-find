e0ce0f8389849f9de68379adebfdfb319e283636
Chris Allison <chris.charles.allison@gmail.com>
2020-02-28 07:01:04 +0000

    adding in switches for project, rss, output dir and help

--- manjaro_torrent_find/mtf.py
+++ manjaro_torrent_find/mtf.py
@@ -1,17 +1,53 @@
 #!/usr/bin/env python3
-"""Manjaro Torrent Find will scrape osdn.net for Manjaro torrents."""
+"""Manjaro Torrent Find will scrape osdn.net for Manjaro torrents.
 
+Usage:
+    mtf [options]
+
+Options:
+    -h this help
+    -o output directory (default: current directory)
+    -p project (either 'manjaro' or 'manjaro-community') (default: both)
+    -r only scrape the OSDN RSS feeds to find the torrent files
+
+With `-o` the output directory must already exist, otherwise the
+current directory is used.
+"""
+
+import os
 import sys
 
-import requests
 from bs4 import BeautifulSoup as BS
+from getopt2 import getopt2
+import requests
+from typing import List
+from typing import Tuple
 
 projects = ["manjaro", "manjaro-community"]
 endings = ["torrent", "sha1", "sha256", "sig"]
 burl = "https://osdn.net"
+outdir = os.getcwd()
+
+
+def usage():
+    sys.exit(f"{__doc__}")
 
 
 def getRSSFeed(rssurl):
+    """Scrapes the RSS feed for filenames.
+
+    Args:
+        rssurl: the rssurl to download
+
+    Returns:
+        dict:
+        {
+        "edition":
+             {"version":
+                 ["file1", "file2", ..., "filen"]
+             }
+        }
+    """
     fns = None
     r = requests.get(rssurl)
     if r.status_code == 200:
@@ -51,7 +87,7 @@ def getEdition(edition, url):
         getVersion(edition[version], f"{url}/{version}")
 
 
-def getProject(project):
+def getRssProject(project):
     purl = f"{burl}/projects/{project}/storage"
     rssurl = f"{purl}/!rss"
     fns = getRSSFeed(rssurl)
@@ -71,21 +107,34 @@ def downloadViaRedirect(fn, url):
             rurl = f"{burl}/{rdir}"
         r = requests.get(rurl)
         if r.status_code == 200:
-            with open(fn, "wb") as ofn:
+            outfn = os.path.abspath("/".join([outdir, fn]))
+            with open(outfn, "wb") as ofn:
                 for chk in r.iter_content():
                     ofn.write(chk)
-            print(f"    {fn} saved ok.")
+            print(f"    {fn} saved ok to {outdir}.")
         else:
             print(f"Failed to download via redirect {fn}: status code: {r.status_code}")
     else:
         print(f"failed to get initial url for {fn} status code: {r.status_code}")
 
 
-def goBabe():
-    for project in projects:
-        print(project)
-        getProject(project)
+@getopt2(sys.argv[1:], "ho:p:r")
+def goBabe(opts: List[Tuple]):
+    dorss = False
+    for opt, arg in opts:
+        if opt == "-h":
+            usage()
+        elif opt == "-o":
+            if os.path.isdir(arg):
+                outdir = arg
+        elif opt == "-p":
+            projects = [arg]
+        elif opt == "-r":
+            dorss = True
+    if dorss:
+        for project in projects:
+            print(f"Obtaining RSS feed for {project}")
+            getRssProject(project)
 
-
-if __name__ == "__main__":
-    goBabe()
+    if __name__ == "__main__":
+        goBabe()
